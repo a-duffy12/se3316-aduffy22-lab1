58726216300651ab1e14c3d2492fb704738860fa
Aiden <12aduffy@gmail.com>
2020-09-11 15:46:56 -0400

    [Feat] Improved links to references
    + References now link directly to the referenced webpage, opening it in a new tab
    + Added the javascript file in the head tag with skeleton functions implemented

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// javascript file
+
+// function to search for a pokemon by name
+function searchName()
+{
+
+}
+
+// function to search for a pokemon by number
+function seachNumber()
+{
+    
+}
