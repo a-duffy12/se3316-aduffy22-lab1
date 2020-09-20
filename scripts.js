aa177c2c52096907024af5b47c3be9285dfe9567
Aiden <12aduffy@gmail.com>
2020-09-20 18:00:50 -0400

    [Fix] Neatened code, few minor style fixes
    ~ Took duplicate code from both search functions out and made it into additional functions to reduce the amount of repetition
    + Updated description to Lab 2 (instead of Lab 1)
    + Added some padding around "Search Results'" header, and aligned search reults' text to the left

--- scripts.js
+++ scripts.js
@@ -15,29 +15,8 @@ function searchNumber()
 
     if ((input.value >= 1) && (input.value <= 20))
     {
-        if (!numActive && !nameActive) // if no search is already active
-        {
-            // create div and ul
-            let newDiv = document.createElement("div");
-            let newUl = document.createElement("ul");
-
-            // set ids for new elements
-            newDiv.setAttribute("id", "results-div");
-            newUl.setAttribute("id", "results-list");
-
-            let newTitle = document.createElement("h1"); // create h1
-            let titleContent = document.createTextNode("Search Results"); // create text
-            newTitle.appendChild(titleContent); // add text to h1
-            newDiv.appendChild(newTitle); // add h1 to div
-            newDiv.appendChild(newUl); // add ul to div
-
-            document.body.insertBefore(newDiv, ul); // add div to DOM
-
-            numActive = true; // number search is active
-        }
-
-        // clear results quickly between each search
-        refreshResults();
+        buildContainer(); // build container for search results
+        refreshResults(); // clear results quickly between each search
 
         for (i=0; i<li.length; i++) // iterate through each list element
         {
@@ -47,47 +26,15 @@ function searchNumber()
 
             if (searchNum.toString().indexOf(input.value.toString()) > -1) // if the number matches a pokemon in the dex
             {
-                // create new corresponding list element 
-                let newLi = document.createElement("li"); // create li
-                let newName = document.createElement("h1"); // create h1
-                let newDesc = document.createElement("h2"); // create h2
-                let newImg = document.createElement("img"); // create img
-
-                // get data from existing pokedex entry
-                let pokemonClass = li[i].className;
-                let pokemonName = li[i].getElementsByTagName("h1")[0].innerText;
-                let pokemonDesc = li[i].getElementsByTagName("h2")[0].innerText;
-                let pokemonImg = li[i].getElementsByTagName("img")[0].src;
-
-                // populate new list element with corresponding data
-                newLi.setAttribute("class", pokemonClass); // ass class to li
-                let nameContent = document.createTextNode(pokemonName); // set pokemon name
-                newName.appendChild(nameContent); // add name to h1
-                let descContent = document.createTextNode(pokemonDesc); // set pokemon description
-                newDesc.appendChild(descContent); // add description to h2
-                newImg.setAttribute("src", pokemonImg); // add image src to img
-                newImg.setAttribute("alt", pokemonName); // add alt image to img
-                
-                // build list element and add it to ul
-                newLi.appendChild(newName); // add h1 to li
-                newLi.appendChild(newDesc); // add h2 to li
-                newLi.appendChild(newImg); // add img to l
-
-                // add this new list element to the ul fo search results
-                let searchList = document.getElementById("results-list");
-                searchList.appendChild(newLi); // add li to ul
+                displayResults(li[i]); // display search results
             }
         }       
     }
     else if (input.value.length == 0) // if there is no search input
     {
-        // remove newly created div and all its children
-        let removeDiv = document.getElementById("results-div");
-        removeDiv.remove();
-        numActive = false; // number search no longer active
-        nameActive = false; // name search no longer active
+        removeContainer(); // remove container for search results
     }
-    else if ((input.value < 1) || (input.value > 20))
+    else if ((input.value < 1) || (input.value > 20)) // unallowed number error
     {
         alert("Error: input must be within 1 and 20"); // error message
     }
@@ -104,29 +51,8 @@ function searchName()
 
     if ((/^[a-zA-Z]+$/.test(searchText)) && (searchText.length <= 20)) // check if search contains only letters
     {
-        if (!nameActive && !numActive)
-        {
-            // create div and ul
-            let newDiv = document.createElement("div");
-            let newUl = document.createElement("ul");
-
-            // set ids for new elements
-            newDiv.setAttribute("id", "results-div");
-            newUl.setAttribute("id", "results-list");
-
-            let newTitle = document.createElement("h1"); // create h1
-            let titleContent = document.createTextNode("Search Results"); // create text
-            newTitle.appendChild(titleContent); // add text to h1
-            newDiv.appendChild(newTitle); // add h1 to div
-            newDiv.appendChild(newUl); // add ul to div
-
-            document.body.insertBefore(newDiv, ul); // add div to DOM
-
-            nameActive = true; // name search is active
-        }
-
-        // clear results quickly between each search
-        refreshResults();
+        buildContainer(); // build container for search results
+        refreshResults(); // clear results quickly between each search
 
         for (i=0; i<li.length; i++) // iterate through each list element
         {
@@ -135,58 +61,63 @@ function searchName()
             
             if (val.toLowerCase().indexOf(searchText) > -1) // if the name is in the search
             {
-                // create new corresponding list element 
-                let newLi = document.createElement("li"); // create li
-                let newName = document.createElement("h1"); // create h1
-                let newDesc = document.createElement("h2"); // create h2
-                let newImg = document.createElement("img"); // create img
-
-                // get data from existing pokedex entry
-                let pokemonClass = li[i].className;
-                let pokemonName = li[i].getElementsByTagName("h1")[0].innerText;
-                let pokemonDesc = li[i].getElementsByTagName("h2")[0].innerText;
-                let pokemonImg = li[i].getElementsByTagName("img")[0].src;
-
-                // populate new list element with corresponding data
-                newLi.setAttribute("class", pokemonClass); // ass class to li
-                let nameContent = document.createTextNode(pokemonName); // set pokemon name
-                newName.appendChild(nameContent); // add name to h1
-                let descContent = document.createTextNode(pokemonDesc); // set pokemon description
-                newDesc.appendChild(descContent); // add description to h2
-                newImg.setAttribute("src", pokemonImg); // add image src to img
-                newImg.setAttribute("alt", pokemonName); // add alt image to img
-
-                // build list element and add it to ul
-                newLi.appendChild(newName); // add h1 to li
-                newLi.appendChild(newDesc); // add h2 to li
-                newLi.appendChild(newImg); // add img to li
-
-                // add this new list element to the ul fo search results
-                let searchList = document.getElementById("results-list");
-                searchList.appendChild(newLi); // add li to ul
+                displayResults(li[i]); // display search results
             }
         } 
     }
     else if (input.value.length == 0) // if there is no search input
     {
-        // remove newly created div and all its children
-        let removeDiv = document.getElementById("results-div");
-        removeDiv.remove();
-        nameActive = false; // name search is no longer active
-        numActive = false; // number search is no longer active
+        removeContainer(); // remove container for search results
     }
-    else if ((/^[a-zA-Z]+$/.test(searchText)) && (searchText.length > 20))
+    else if ((/^[a-zA-Z]+$/.test(searchText)) && (searchText.length > 20)) // too many characters error
     {
         alert("Error: input cannot exceed 20 characters"); // error message
     }
-    else if (searchText.length <= 20)
+    else if (searchText.length <= 20) // unallowed character error
     {
         alert("Error: input can only include characters A-Z and a-z"); // error message
     }
-    else if (searchText.length > 20)
+    else if (searchText.length > 20) // unallowed character error and too many character error
     {
         alert("Error: input can only include characters A-Z and a-z and cannot exceet 20 characters"); // error message
     }
+}
+
+// function to dynamically create the container for search results
+function buildContainer()
+{
+    pokeList = document.getElementById("pokemon-list"); // get pokedex list element
+
+    if (!nameActive && !numActive)
+    {
+        // create div and ul
+        let newDiv = document.createElement("div");
+        let newUl = document.createElement("ul");
+
+        // set ids for new elements
+        newDiv.setAttribute("id", "results-div");
+        newUl.setAttribute("id", "results-list");
+
+        let newTitle = document.createElement("h1"); // create h1
+        let titleContent = document.createTextNode("Search Results"); // create text
+        newTitle.appendChild(titleContent); // add text to h1
+        newDiv.appendChild(newTitle); // add h1 to div
+        newDiv.appendChild(newUl); // add ul to div
+
+        document.body.insertBefore(newDiv, pokeList); // add div to DOM
+
+        nameActive = true; // name search is active
+    }
+}
+
+// function to dynamically remove the container for search results
+function removeContainer()
+{
+    // remove newly created div and all its children
+    let removeDiv = document.getElementById("results-div");
+    removeDiv.remove();
+    nameActive = false; // name search is no longer active
+    numActive = false; // number search is no longer active
 }
 
 // function to clear search results in between searchs 
@@ -198,4 +129,32 @@ function refreshResults()
     {   
         rl.removeChild(rl.firstChild); // remove first list element
     }
+}
+
+// function to build list of search results based on existing pokedex list
+function displayResults(el)
+{
+    // create new corresponding list element 
+    let newLi = document.createElement("li"); // create li
+    let newName = document.createElement("h1"); // create h1
+    let newDesc = document.createElement("h2"); // create h2
+    let newImg = document.createElement("img"); // create img
+
+    // populate new list element with corresponding data
+    newLi.setAttribute("class", el.className); // get class name and add class name to li
+    let nameContent = document.createTextNode(el.getElementsByTagName("h1")[0].innerText); // get pokemon name and create a text node for it
+    newName.appendChild(nameContent); // add name to h1
+    let descContent = document.createTextNode(el.getElementsByTagName("h2")[0].innerText); // get pokemon description and create a text node for it
+    newDesc.appendChild(descContent); // add description to h2
+    newImg.setAttribute("src", el.getElementsByTagName("img")[0].src); // get image src and add image src to img
+    newImg.setAttribute("alt", el.getElementsByTagName("h1")[0].innerText); // use pokemon name as image alt and add image alt to img
+
+    // build list element and add it to ul
+    newLi.appendChild(newName); // add h1 to li
+    newLi.appendChild(newDesc); // add h2 to li
+    newLi.appendChild(newImg); // add img to li
+
+    // add this new list element to the ul fo search results
+    let searchList = document.getElementById("results-list");
+    searchList.appendChild(newLi); // add li to ul
 }
